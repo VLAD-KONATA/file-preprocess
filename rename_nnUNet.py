c8216c423cccbcdfedec2e65dca1b78824fef58a
VLAD-KONATA <mfyang2000@qq.com>
2024-01-04 12:44:09 +0800

    批量重命名（nnUNetv2）

--- rename_nnUNet.py
+++ rename_nnUNet.py
@@ -1,26 +1,26 @@
 import os
 import shutil
-baseDir = os.path.normpath(r'D:\data\w_label(err')
-objDir = os.path.normpath(r'D:\data\w_label_test')
+baseDir = os.path.normpath(r'/home/bo/Gao_win/nnUNet/DATASET/nnUNet_raw/Dataset012_Breast/rawdata')
+objDir = os.path.normpath(r'/home/bo/Gao_win/nnUNet/DATASET/nnUNet_raw/Dataset012_Breast')
 folders=os.listdir(baseDir)
 name=['Breast.nii.gz','P0.nii.gz','P1_reg.nii.gz','Tissues.nii.gz']
 for folder in folders:
-    files=os.listdir(baseDir+"\\"+folder)
+    files=os.listdir(baseDir+"/"+folder)
     print(folder[-3:])
     print(files)
     for file in files:
-        os.chdir(baseDir+"\\"+folder)
+        os.chdir(baseDir+"/"+folder)
         if file==name[0]:
-            shutil.copy(baseDir+"\\"+folder+"\\"+file,objDir+"\\"+"breast")
-            os.chdir(objDir+"\\"+"breast")
+            shutil.copy(baseDir+"/"+folder+"/"+file,objDir+"/"+"imagesTr(b")
+            os.chdir(objDir+"/"+"imagesTr(b")
             os.rename(file,"Breast_"+folder[-3:]+".nii.gz")
         elif file==name[1]:
-            shutil.copy(baseDir+"\\"+folder+"\\"+file,objDir+"\\"+"raw")
-            os.chdir(objDir+"\\"+"raw")
+            shutil.copy(baseDir+"/"+folder+"/"+file,objDir+"/"+"imagesTr")
+            os.chdir(objDir+"/"+"imagesTr")
             os.rename(file,"Breast_"+folder[-3:]+"_0000.nii.gz")
         elif file==name[3]:
-            shutil.copy(baseDir+"\\"+folder+"\\"+file,objDir+"\\"+"tissue")
-            os.chdir(objDir+"\\"+"tissue")
+            shutil.copy(baseDir+"/"+folder+"/"+file,objDir+"/"+"labelsTr")
+            os.chdir(objDir+"/"+"labelsTr")
             os.rename(file,"Breast_"+folder[-3:]+".nii.gz")
 
     
